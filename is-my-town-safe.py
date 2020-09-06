4e15080f7f4989ce2a45fe5f8434e2ba29e9f0a7
Tristan Weir <20230428+tristanweir@users.noreply.github.com>
2020-09-06 10:27:48 -0700

    Add basic calculations

--- is-my-town-safe.py
+++ is-my-town-safe.py
@@ -26,7 +26,9 @@ def filterData(data, zipCodesToKeep):
 
 '''
 data1 and data2 are dicts that look like
-{ zip : { some info about the zip }}
+{ key : { some info about the key }}
+
+In most cases the keys are the same in each dict, but they don't have to be
 '''
 def mergeData(data1, data2):
     for key in data2:
@@ -37,12 +39,23 @@ def mergeData(data1, data2):
     return data1
 
 
+def aggregate(data, value_to_aggregate):
+    running_total = 0
+
+    for key in data:
+        if value_to_aggregate in data[key]:
+            running_total += data[key][value_to_aggregate]
+
+    return running_total
+
+
 def main():
-    zipCodesToKeep = [94601, 94602]
+    zipCodesToKeep = [94601, 94602, 94606, 94610, 94619]
     
     url1 = "https://services5.arcgis.com/ROBnTHSNjoZ2Wm1P/arcgis/rest/services/COVID_19_Statistics/FeatureServer/0/query?where=1%3D1&outFields=Zip_Number,Population,Cases,CaseRates&returnGeometry=false&outSR=4326&f=json"
     url2 = "https://services5.arcgis.com/ROBnTHSNjoZ2Wm1P/arcgis/rest/services/COVID_19_Statistics/FeatureServer/1/query?where=1%3D1&outFields=Zip_Number,PercentagePositiveTests&returnGeometry=false&outSR=4326&f=json"
 
+    '''
     data1 = pullData(url1)
     filteredData1 = filterData(data1["features"], zipCodesToKeep)
     
@@ -50,9 +63,21 @@ def main():
     filteredData2 = filterData(data2["features"], zipCodesToKeep)
     
     
-    print(filteredData1)
-    print(filteredData2)
+    #print(filteredData1)
+    #print(filteredData2)
     merged = mergeData(filteredData1, filteredData2)
     print(merged)
+    '''
+    # for testing, statically load the merged dataset so we are not constantly calling the API
+    merged = {94606: {'Zip_Number': 94606, 'Population': 38169, 'Cases': 639, 'CaseRates': 1674.13345909, 'PercentagePositiveTests': 5.964795194794}, 94610: {'Zip_Number': 94610, 'Population': 30014, 'Cases': 154, 'CaseRates': 513.09388952, 'PercentagePositiveTests': 1.581609195402}, 94619: {'Zip_Number': 94619, 'Population': 25119, 'Cases': 348, 'CaseRates': 1385.40546996, 'PercentagePositiveTests': 5.819360293081}, 94601: {'Zip_Number': 94601, 'Population': 55840, 'Cases': 2035, 'CaseRates': 3644.34097421, 'PercentagePositiveTests': 15.088161209068}, 94602: {'Zip_Number': 94602, 'Population': 30831, 'Cases': 311, 'CaseRates': 1008.72498459, 'PercentagePositiveTests': 3.97580916116}}
+    print(merged)
+
+    total_cases = aggregate(merged,"Cases")
+    total_population = aggregate(merged, "Population")
+    case_rate = total_cases / total_population * 100000
+
+    print("Total Cases:", format(total_cases, ',d'))
+    print("Total Population:", format(total_population, ',d'))
+    print("Case Rate per 100,000:", format(case_rate, ',.1f'))  # format to 1 decimal place
 
 main()
