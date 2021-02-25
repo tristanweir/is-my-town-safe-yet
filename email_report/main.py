4c7c2aa7dc5f4ec4ab9da46c7d5274f35f1cb09f
Tristan Weir <tweir@google.com>
2021-02-25 09:06:23 -0800

    Clean up debug, comments and readibility

--- email_report/main.py
+++ email_report/main.py
@@ -71,9 +71,10 @@ def create_body():
 
     return body
 
+# TODO: Convert to GCP Cloud Function
 def main():
 
-    # TODO: Parse emails from inbound HTTP request
+    # TODO: Parse email addresses from inbound HTTP request object
     from_temp = ""
     send_to = ""
     subject = "COVID-19 Report for " + str(date.today())
@@ -96,3 +97,26 @@ def main():
     # print(body)    
 
 main()
+
+
+
+
+# reference for processing inbound flask requests
+'''
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
+'''
